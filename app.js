4c6b8541a9f47c33e404ebf13881928eeb92fdc2
Andre Barros <andrebarros7201@gmail.com>
2024-07-23 19:04:30 +0100

    add style.css

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const path = require("path");
 const app = express();
 
 const messageRouter = require("./routes/messages");
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
